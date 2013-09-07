3c16d7e40172c4196635a158465a1c97d62ddfaa
Donald Huang <don.hcd@gmail.com>
2013-09-07 03:35:58 -0400

    you need some files

---
+++ assets/js/feedasana.js
@@ -0,0 +1,44 @@
+/**
+ * The angular controller for feedasana.
+ */
+feedController = function($scope) {
+  $scope.allSubscriptions = [
+    {
+      owner: false,
+      name: "15-462: Graphics",
+      tasks: [
+        {name:"pool", due:"Oct 12, 2014"},
+        {name:"ray tracer", due:"Nov 23, 2014"},
+        {name:"shader", due:"Dec 02, 2014"}
+      ]
+    },
+    {
+      owner: false,
+      name: "15-466: Photography",
+      tasks: [
+        {name:"p1 and friends", due:"Oct 12, 2015"},
+        {name:"p2 yea...", due:"Nov 23, 2015"},
+        {name:"p5 killer", due:"Dec 02, 2018"}
+      ]
+    },
+    {
+      owner: true,
+      name: "Don's killer workout",
+      tasks: [
+        {name:"P90X", due:"May 12, 2015"},
+        {name:"50 push ups", due:"Dec 30, 2100"}
+      ]
+    }
+  ];
+
+  $scope.curFeed;
+
+  $scope.setCurFeed = function(feed) {
+    $scope.curFeed = feed;
+  }
+
+  $scope.saveTask = function(name, duedate) {
+    window.console.log(name);
+    window.console.log(duedate);
+  }
+}
