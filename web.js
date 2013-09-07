8139e84221ac81fe6398fac338b3d080ec2c7a4c
Donald Huang <don.hcd@gmail.com>
2013-09-07 09:06:29 -0400

    made some endpoints and stuff hope it works :)

--- web.js
+++ web.js
@@ -25,76 +25,112 @@ app.use(express.logger());
 app.use(express.cookieParser());
 app.use(express.session({secret: process.env.BEST_SECRET_EVER}));
 app.use(express.bodyParser());
-app.use(express.static(__dirname + '/assets'));
+app.use(express['static'](__dirname + '/assets'));
 
-/** Define feed model for database */
-feedSchema = mongoose.Schema({
-  name: String, // name of feed
-  owner: String, // owner for feed
-  tasks: [{shortDesc : String, fullDesc : String, date : Date}] // pending tasks
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+UserSchema = new Schema({
+  name: String,
+  access_token: Object,
+  feeds: [{ type: ObjectId, ref: 'FeedSchema' }],
+  subscriptions: [{ type: ObjectId, ref: 'FeedSchema' }]
 });
-var Feed = mongoose.model('Feed', feedSchema);
+FeedSchema = Schema({
+  name: String, // name of feed
+  owner: { type: ObjectId, ref: 'UserSchema' }, // owner for feed
+  tasks: { type: ObjectId, ref: 'TaskSchema' },
+  subscribers: [{ type: ObjectId, ref: 'UserSchema' }]
+});
+TaskSchema = Schema({
+  name: String,
+  notes: String,
+  due_date: Date,
+  feed: { type : ObjectId, ref: 'FeedSchema' }
+});
+
+var User = mongoose.model('User', UserSchema),
+    Feed = mongoose.model('Feed', FeedSchema),
+    Task = mongoose.model('Task', TaskSchema),
+    db = mongoose.connection;
 
 /* Connect to database */
 mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
 
 app.get('/', function(request, response) {
-  var access_token = request.session.access_token;
-  if (typeof access_token !== 'undefined') {
-    asana.setResourceOwner(access_token);
-    asana.getUserMe(null, function(error, me) {
-      if (error) {
-        console.log(error);
-        response.send('yo whattup homy we gots problem '+error);
-      } else {
-        response.send(ejs.render(index_str, {penis:'hi'}));
-          // 'yo whattup homy' + me.data.name);
-      }
-    });
+  var user = request.session.user;
+  if (typeof user !== 'undefined') {
+    response.send(ejs.render(index_str, {penis:'hi'}));
   } else {
-    // give middle page thingy
+    // TODO: give middle page thingy
     response.redirect(authorization_uri);
   }
 });
 
 app.post('/feeds', function(request, response) {
-  var access_token = request.session.access_token;
-  if (typeof access_token === 'undefined') {
+  var user = request.session.user;
+  if (typeof user === 'undefined') {
     return response.send({success:false});
   }
-  asana.setResourceOwner(access_token);
-  asana.getUserMe(null, withMe);
 
-  function withMe(error, me) {
-    if (error) {
-      response.send({
-        success: false,
-        error: error // "error getting me from asana"
-      });
-    } else {
-      var feed = new Feed({
-        name: request.body.name,
-        owner: request.body.owner,
-        tasks : []
-      });
-      feed.save(withSavedFeed);
-    }
-  }
-
-  function withSavedFeed(error, saved_feed) {
+  var feed = new Feed({
+    name: request.body.name,
+    owner: user.id,
+    tasks : []
+  });
+  feed.save(function withSavedFeed(error, saved_feed) {
     if (error) {
       response.send({
         success: false,
         error: error
       });
     } else {
-      response.send({
-        success: true,
-        feed: saved_feed
+      user.update({$push: {feeds: saved_feed.id}}, function(error, num_affected, raw_response) {
+        if (err) return res.send('error adding feed to user ' + err);
+        console.log('added feed to user');
+        response.send({
+          success: true,
+          feed: saved_feed
+        });
       });
     }
+  });
+});
+
+app.post('/tasks', function(request, response) {
+  var user = request.session.user;
+  if (typeof user === 'undefined') {
+    return response.send({success:false});
   }
+  Feed.findOne({ _id: request.body.feed_id }).populate('owner').exec(function(error, feed) {
+    console.log('feed = ' + feed);
+    if (typeof feed === 'undefined' || feed.owner !== user.id) {
+      return response.send({
+        success: false,
+        error: 'can\'t find this feed: ' + error
+      });
+    }
+
+    var task = new Task({
+      name: request.body.name,
+      notes: request.body.notes,
+      due_date: new Date(request.body.due_date),
+      feed: request.body.feed_id
+    });
+    task.save(function(error, saved_task) {
+      if (error) return response.send({ success: false, error: 'can\'t save task: ' + error });
+
+      feed.update({$push: {tasks: saved_task.id}}, function(error, num_affected, raw_response) {
+        if (err) return res.send('error adding feed to user ' + err);
+        console.log('added feed to user');
+        response.send({
+          success: true,
+          task: saved_task
+        });
+      });
+    });
+  });
+  asana.setResourceOwner(user.access_token);
 });
 
 app.get('/callback', function(request, response) {
@@ -102,17 +138,42 @@ app.get('/callback', function(request, response) {
   OAuth2.AuthCode.getToken({
     code: code,
     redirect_uri: callback_uri
-  }, saveToken);
+  }, withToken);
 
-  function saveToken(error, result) {
+  function withToken(error, token_result) {
     if (error) {
       console.log('Access Token Error', error.message);
       response.send('bad problem yo');
     } else {
-      var token = OAuth2.AccessToken.create(result);
-      request.session.access_token = token;
-      console.log('token is', token);
-      response.redirect('/');
+      var access_token = OAuth2.AccessToken.create(token_result);
+      console.log('token is', access_token);
+      asana.setResourceOwner(access_token);
+      asana.getUserMe(null, function(error, me) {
+        if (error) {
+          console.log(error);
+          response.send('yo whattup homy we gots problem '+error);
+        } else {
+          var user = new User({
+            name: me.data.name,
+            access_token: access_token,
+            feeds: [],
+            subscriptions: []
+          });
+          user.save(withSavedUser);
+        }
+
+        function withSavedUser(error, saved_user) {
+          if (error) {
+            response.send({
+              success: false,
+              error: error
+            });
+          } else {
+            request.session.user = saved_user;
+            response.redirect('/');
+          }
+        }
+      });
     }
   }
 });
