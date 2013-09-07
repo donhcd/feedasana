d19829b256b105945fe2e01f30d88bb000b99845
Susan Wang <susanwang1015@gmail.com>
2013-09-06 23:09:22 -0400

    susan: added some dummy data, just open index.html

--- html/js/feedasana.js
+++ html/js/feedasana.js
@@ -1,5 +1,37 @@
-
-
+/**
+ * The angular controller for feedasana.
+ */
 feedController = function($scope) {
   $scope.name = "Welcome to feed asana";
+
+  $scope.allSubscriptions = [
+    {
+      ownerid: "123",
+      name: "15-462: Graphics",
+      tasks: [
+        {name:"pool", due:"Oct 12, 2014"},
+        {name:"ray tracer", due:"Nov 23, 2014"},
+        {name:"shader", due:"Dec 02, 2014"}
+      ]
+    },
+    {
+      ownerid: "456",
+      name: "15-466: Photography",
+      tasks: [
+        {name:"p1 and friends", due:"Oct 12, 2015"},
+        {name:"p2 yea...", due:"Nov 23, 2015"},
+        {name:"p5 killer", due:"Dec 02, 2018"}
+      ]
+    },
+    {
+      ownerid: "789",
+      name: "Don's killer workout",
+      tasks: [
+        {name:"P90X", due:"May 12, 2015"},
+        {name:"50 push ups", due:"Dec 30, 2100"}
+      ]
+ 
+
+    }
+  ];
 }
